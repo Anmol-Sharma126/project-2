0ceaa5c6255b9620e89dbbf63a836bd3aa6a95ec
Anmol-Sharma126 <122034601+Anmol-Sharma126@users.noreply.github.com>
2023-02-23 12:58:56 +0530

    Update and rename AtmMangmentsys.py.txt to AtmMangmentsys.py

--- AtmMangmentsys.py
+++ AtmMangmentsys.py
@@ -117,4 +117,20 @@ while (True):
                         print("Process Cancelled!\n\n")
 
                     break
+        else:
+            num_of_tries-=1
+            print("PIN incorrect! Number of tries left -", num_of_tries, end = "\n\n")
+
+    else:
+        print("Exiting...")
+
+        t.sleep(2)
+        print("You have been logged out. Thank you!\n\n")
+        break
+
+
+else:
+    print("Invalid input!")
+    print("\t\t0. Enter 0 to Logout and Exit!")
+    continue                   
 
