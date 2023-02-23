757a650b86c42c0ea864a2ce3b3363d45bf78395
Anmol-Sharma126 <122034601+Anmol-Sharma126@users.noreply.github.com>
2023-02-23 13:20:39 +0530

    Update AtmMangmentsys.py

--- AtmMangmentsys.py
+++ AtmMangmentsys.py
@@ -122,5 +122,16 @@ while (True):
             print("PIN incorrect! Number of tries left -", num_of_tries, end = "\n\n")
 
 
-          
+        else:
+            print("Exiting...")
+
+            t.sleep(2)
+            print("You have been logged out. Thank you!\n\n")
+            break
+
+
+    else:
+        print("Invalid input!")
+        print("\t\t0. Enter 0 to Logout and Exit!")
+        continue
 
